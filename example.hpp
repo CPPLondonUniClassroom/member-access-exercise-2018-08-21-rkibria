3fa461be7593829771394732451b6873ee0b8c60
Raihan Kibria <4503850+rkibria@users.noreply.github.com>
2018-08-21 20:49:59 +0100

    solution

--- example.hpp
+++ example.hpp
@@ -8,7 +8,32 @@
 #include <iostream>
 #include <string>
 
-struct student {
+#include <cctype>
+#include <clocale>
+
+class student {
+	public:
+		student(const std::string& first, const std::string& last, int id)
+			: first_name{first}, last_name{last}, id_number{id}
+		{}
+		
+		const std::string& get_first_name() const {return first_name;}
+		const std::string& get_last_name() const {return last_name;}
+		int get_id_number() const {return id_number;}
+
+		void set_first_name(const std::string& first)
+		{
+			first_name = first;
+			first_name[0] = toupper(first_name[0]);
+			for(size_t i=1; i < first_name.length(); ++i)
+			{
+				first_name[i] = tolower(first_name[i]);
+			}
+		}
+
+	private:
+		friend std::ostream& operator<<(std::ostream& os, const student& s);
+	
     std::string first_name;
     std::string last_name;
     int id_number;
--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@ int main()
 {
     std::vector<student> students;
 
-    students.push_back(student{"Harry", "Potter", 1});
-    students.push_back(student{"Ron", "Weasley", 2});
-    students.push_back(student{"Hermione", "Granger", 3});
+    students.push_back(student("Harry", "Potter", 1));
+    students.push_back(student("Ron", "Weasley", 2));
+    students.push_back(student("Hermione", "Granger", 3));
 
+		students[0].set_first_name("tEST");
+		
     for (const auto& s : students) {
         std::cout << s << '\n';
     }
